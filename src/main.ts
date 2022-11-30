a5be69688f300bbfbc54c9a7eed23294b3d816e3
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-11-30 21:05:56 +0900

    todoの追加と削除のロジックの追加

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,20 @@ import { Quasar, Dialog, Notify } from "quasar";
 import quasarLang from "quasar/lang/ja";
 
 import App from "./App.vue";
-import router from "./router";
+// import router from "./router";
 
 // Import quasar CSS
-import "@quasar/extras/material-icons/material-icons.css";
-import "quasar/src/css/index.sass";
+// import "@quasar/extras/material-icons/material-icons.css";
+// import "quasar/src/css/index.sass";
 
 const app = createApp(App);
 
 app.use(createPinia());
-app.use(router);
+// app.use(router);
 // Setup quasar
-app.use(Quasar, {
-  plugins: [Dialog, Notify],
-  lang: quasarLang,
-});
+// app.use(Quasar, {
+//   plugins: [Dialog, Notify],
+//   lang: quasarLang,
+// });
 
 app.mount("#app");
--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -1,26 +1,56 @@
 <script setup lang="ts">
-import { ref } from "vue-demi"
-let id:number = 0
-const todos = ref([
-  {id: id++, todo: "test", done: false},
-  {id: id++, todo: "test", done: false},
-  {id: id++, todo: "test", done: false}
-])
+import { ref } from "vue-demi";
 
+type TODO = {
+  id: number;
+  todo: string;
+  done: boolean;
+}
+let id: number = 0;
+const todos = ref([
+  { id: id++, todo: "test", done: false },
+  { id: id++, todo: "test", done: false },
+  { id: id++, todo: "test", done: false },
+]);
+
+const text = ref("")
+
+const  addTodo =() =>   {
+  todos.value.push( { id: id++, todo: text.value, done: false });
+  text.value = ""
+
+
+};
+
+const removeTodo = ( todo:TODO):void => {
+ todos.value = todos.value.filter((t) => {
+    return t !== todo
+  })
+}
 </script>
 <template>
   <p-page>
     <h1 class="">TodoApp</h1>
-    <input type="text" >
 
+    <form @submit.prevent="addTodo">
+      <input v-model="text"  />
+
+      <button >todoの追加</button>
+    </form>
+    {{text}}
     <ul>
-      <li></li>
+      <li v-for="todo in todos" :key="todo.id">
+        {{ todo.todo }}
+        <button @click="removeTodo(todo)">X</button>
+      </li>
     </ul>
   </p-page>
 </template>
 
 <style>
 h1 {
-  font-size: 36px
+  font-size: 36px;
 }
 </style>
+
+
